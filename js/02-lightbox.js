77f56fb4bb1cc04de8d337dde6c80b5e28915b99
galynary <107271972+galynary@users.noreply.github.com>
2022-11-20 19:57:44 +0200

    work on the task 7-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,49 +1,33 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+console.log(galleryItems);
 
-console.log(galleryItems);
-import { galleryItems } from "./gallery-items.js";
-console.log(galleryItems);
-const divRef = document.querySelector(".js-gallery");
+const divRef = document.querySelector(".gallery");
 function createGallaryMarkup(items) {
   return items
     .map(
       (item) =>
-        `<div class="gallery__item">
-      <a
-        class="gallery__link"
-        href ="${item.original}";
+        `<a class="gallery__item"
+      href ="${item.original}";
       >
         <img
           class="gallery__image"
           src="${item.preview}"
-          data-source="${item.original}"
-          alt="${item.description}"
+         alt="${item.description}"
         />
       </a>
-    </div>`
+    `
     )
     .join("");
 }
-const addGallaryMarkup = createGallaryMarkup(galleryItems);
-divRef.innerHTML = addGallaryMarkup;
-divRef.addEventListener("click", onImageClick);
 function onImageClick(evt) {
-  blockStandartAction(evt);
-  if (evt.target.nodeName !== "IMG") {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__item")) {
     return;
   }
-  const instance = basicLightbox.create(
-    `<img src="${evt.target.dataset.source}" width ="800" heigth ="600">`
-  );
-  instance.show();
 
-  divRef.addEventListener("keydown", (evt) => {
-    if (evt.code === "Escape") {
-      instance.close();
-    }
+  const lightbox = new SimpleLightbox(".gallery__item", {
+    captionsData: "alt",
+    captionDelay: 250,
   });
-}
-function blockStandartAction(evt) {
-  evt.preventDefault();
 }
